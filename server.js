ecc536b1f675013e58d1a22148f4acd22197958c
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-30 15:35:33 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 // Define the port to run the server on
 const PORT = 80;
 
-// Function to check the hostname
+// Function to check the hostname (for both www and non-www versions)
 const checkHostname = (hostname, req) => {
-    return req.hostname === hostname || req.hostname === `www.${hostname}`;
+    return [hostname, `www.${hostname}`].includes(req.hostname);
 };
 
 // Define the response for "prestonbrubaker.com"
@@ -29,10 +29,10 @@ app.use((req, res, next) => {
 
 // Fallback for any other requests
 app.use((req, res) => {
-    res.send('Neither Preston or Willoh');
+    res.send('This is a generic response for other domains or requests.');
 });
 
-// Start the server
-app.listen(PORT, () => {
+// Start the server and listen on all network interfaces
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
