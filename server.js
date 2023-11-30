f2c7ba09684b439d3d4e7646f8fa3afa7bdc752a
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-30 15:47:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// Define the port to run the server on
-const PORT = 58500;
+app.use(express.static(__dirname)); // Serve static files from the current directory
 
-// A simple route that responds to any request
-app.use((req, res) => {
-    res.send('Welcome to the server accessible via IP address 73.166.159.150 on port 58500!');
+let chatLog = []; // In-memory storage for chat messages
+let rectangles = []; // In-memory storage for rectangles
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (data) => {
+        chatLog.push(data); // Add message to chat log
+        io.emit('chat message', data); // Broadcast message to all connected clients
+    });
+
+    socket.on('new rectangle', (data) => {
+        rectangles.push(data); // Add new rectangle data
+        io.emit('update rectangles', rectangles); // Broadcast updated rectangles to all connected clients
+    });
 });
 
-// Start the server and listen on all network interfaces
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 58541; // Use the port from the environment variable or default to 58541
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
