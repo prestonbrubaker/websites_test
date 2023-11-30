e5b59c1d2706f6f30642ffecd3b6f85b36735aeb
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-30 15:15:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 80;
 
 // Define the response for "prestonbrubaker.com"
 app.use((req, res, next) => {
-    if (req.hostname === 'prestonbrubaker.com') {
+    if (req.hostname === 'prestonbrubaker.com' || req.hostname === 'http://prestonbrubaker.com') {
         res.send('Welcome to Preston Brubaker\'s website!');
     } else {
         next();
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 // Define the response for "willohrobbins.com"
 app.use((req, res, next) => {
-    if (req.hostname === 'willohrobbins.com') {
+    if (req.hostname === 'willohrobbins.com' || req.hostname === 'http://willohrobbins.com') {
         res.send('Welcome to Willoh Robbins\' website!');
     } else {
         next();
