d877030495fcf5d0d655b61f0eff73f7ceceadaf
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-30 16:06:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello, World!</h1>');
 });
 
 const PORT = 80;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
