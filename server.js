9d8510327e83d838ae6daa9a4b94817ed41fec6d
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-30 14:45:53 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+// Define the port to run the server on
+const PORT = 80;
+
+// Define the response for "prestonbrubaker.com"
+app.use((req, res, next) => {
+    if (req.hostname === 'prestonbrubaker.com') {
+        res.send('Welcome to Preston Brubaker\'s website!');
+    } else {
+        next();
+    }
+});
+
+// Define the response for "willohrobbins.com"
+app.use((req, res, next) => {
+    if (req.hostname === 'willohrobbins.com') {
+        res.send('Welcome to Willoh Robbins\' website!');
+    } else {
+        next();
+    }
+});
+
+// Fallback for any other requests
+app.use((req, res) => {
+    res.status(404).send('Website not found');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
