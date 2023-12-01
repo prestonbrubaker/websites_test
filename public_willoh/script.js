da376c3135d9b136a42736f4cf8f5529c9ef84da
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-30 22:40:14 -0600

    Update script.js

--- public_willoh/script.js
+++ public_willoh/script.js
@@ -1,43 +1,23 @@
-// Basic JavaScript for interactive elements
 document.addEventListener('DOMContentLoaded', () => {
-    // JavaScript code goes here
+    var hue = 0;
+
+    // Accessing the canvas element
+    var canvas = document.getElementById('canvas1');
+    var ctx = canvas.getContext('2d');
+
+    function incrementHue() {
+        // Increment the hue
+        hue = (hue + 1) % 360; // This will cycle hue from 0 to 359
+
+        // Set the fill color using HSL
+        var color_rect = "hsl(" + hue + ", 100%, 50%)";
+        ctx.fillStyle = color_rect;
+
+        // Clear the canvas and draw a new rectangle
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(50, 50, 100, 100);
+    }
+
+    // Call incrementHue every 100 milliseconds (0.1 seconds)
+    setInterval(incrementHue, 100);
 });
-
-var hue = 0
-
-// Accessing the canvas element
-var canvas = document.getElementById('canvas1');
-var ctx = canvas.getContext('2d');
-
-
-function incrementHue() {
-    // Get the element
-    var element = document.getElementById("dynamic-color");
-
-    // Extract the current hue value from the element's color
-    var color = window.getComputedStyle(element).color;
-    var hsl = color.match(/\d+/g); // This extracts numbers from the RGB color
-    var saturation = 50;
-    var lightness = 50;
-
-    // Increment the hue
-    hue += 1
-
-    // Update the element's color
-    element.style.color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-
-
-    // Set the fill color using HSL
-    // Example: HSL with 200 hue, 100% saturation, 50% lightness
-    var color_rect = "hsl(" + hue + ", 100%, 50%)";
-    ctx.fillStyle = color_rect;
-    
-    // Draw a rectangle
-    // fillRect(x, y, width, height)
-    ctx.fillRect(50, 50, 100, 100);
-
-}
-
-// Call incrementHue every 100 milliseconds (0.1 seconds)
-setInterval(incrementHue, 10);
-
