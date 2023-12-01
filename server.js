4c66b184b3bd22e637e7cb4e49ec0526787e55e7
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-30 19:30:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello, World!</h1>');
+// Define the port to run the server on
+const PORT = 80;
+
+// Function to check the hostname (for both www and non-www versions)
+const checkHostname = (hostname, req) => {
+    return [hostname, `www.${hostname}`].includes(req.hostname);
+};
+
+// Define the response for "prestonbrubaker.com"
+app.use((req, res, next) => {
+    if (checkHostname('prestonbrubaker.com', req)) {
+        res.send('Welcome to Preston Brubaker\'s website!');
+    } else {
+        next();
+    }
 });
 
-const PORT = 80;
-server.listen(PORT, () => {
+// Define the response for "willohrobbins.com"
+app.use((req, res, next) => {
+    if (checkHostname('willohrobbins.com', req)) {
+        res.send('Welcome to Willoh Robbins\' website!');
+    } else {
+        next();
+    }
+});
+
+// Fallback for any other requests
+app.use((req, res) => {
+    res.send('This is a generic response for other domains or requests.');
+});
+
+// Start the server and listen on all network interfaces
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
