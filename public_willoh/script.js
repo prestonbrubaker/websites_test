e0a8c80deff0cf2091472b8ca2e40ce9b4b76b43
willohrobbins <willoh_robbins@outlook.com>
2023-12-02 14:40:29 -0600

    ombre

--- public_willoh/script.js
+++ public_willoh/script.js
@@ -1,79 +1,68 @@
-let flowers = [];
+let startTime;
+let colorStopsStart = []; // Start gradient colors
+let colorStopsEnd = []; // End gradient colors
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    let flowerCount = 20; // Adjust the number of flowers as needed
+  createCanvas(windowWidth, windowHeight);
+  noLoop(); // We will manually control the drawing loop
+  colorMode(RGB); // Use RGB color mode
+  startTime = millis();
 
-    // Create initial set of flowers
-    for (let i = 0; i < flowerCount; i++) {
-        flowers.push(new Flower(random(width), height - 20, randomRedOrPink()));
-    }
+  // Manually define color stops for the starting ombre (sunrise)
+  colorStopsStart = [
+    // Replace these colors with those from your sunrise image
+    color(255, 189, 109), 
+    color(255, 112, 67),  
+    color(255, 84, 78),   
+  ];
+
+  // Manually define color stops for the ending ombre (sunset)
+  colorStopsEnd = [
+    // Replace these colors with those from your sunset image
+    color(64, 164, 223), 
+    color(75, 119, 190), 
+    color(52, 92, 150),  
+  ];
 }
 
 function draw() {
-    background(255);
+  let cycleDuration = 60000; // 1 minute cycle for a full transition
+  let halfCycle = cycleDuration / 2; // Half cycle for transition back
+  let elapsedTime = (millis() - startTime) % cycleDuration;
+  let lerpValue;
 
-    // Draw and update each flower
-    for (let flower of flowers) {
-        flower.update();
-        flower.display();
-    }
-}
-
-// Flower class
-class Flower {
-    constructor(x, y, color) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-        this.size = 0; // Starting size of the flower
-        this.growthRate = random(0.1, 0.3); // How fast the flower grows
-        this.maxSize = random(20, 40); // Maximum size of the flower
-        this.stemHeight = 0; // Starting height of the stem
-        this.maxStemHeight = random(50, 100); // Maximum height of the stem
-        this.petals = floor(random(5, 8)); // Number of petals
-    }
-
-    update() {
-        // Grow the flower and the stem
-        if (this.size < this.maxSize) {
-            this.size += this.growthRate;
-        }
-        if (this.stemHeight < this.maxStemHeight) {
-            this.stemHeight += this.growthRate / 2;
-        }
-    }
-
-    display() {
-      // Draw the stem
-      stroke(35, 177, 77); // Green for the stem
-      strokeWeight(2);
-      line(this.x, this.y, this.x, this.y - this.stemHeight);
-  
-      // Draw the flower
-      fill(this.color);
-      noStroke(); // Disable stroke for the petals
-      for (let i = 0; i < this.petals; i++) {
-          push();
-          translate(this.x, this.y - this.stemHeight);
-          rotate(TWO_PI / this.petals * i);
-          ellipse(0, this.size / 4, this.size / 2, this.size); // Petal shape
-          pop();
-      }
-  
-      // Draw the center of the flower
-      fill(255, 204, 0); // Yellow center
-      ellipse(this.x, this.y - this.stemHeight, this.size / 4);
+  if (elapsedTime < halfCycle) {
+    // First half of the cycle - transition from start to end colors
+    lerpValue = map(elapsedTime, 0, halfCycle, 0, 1);
+  } else {
+    // Second half of the cycle - transition from end back to start colors
+    lerpValue = map(elapsedTime, halfCycle, cycleDuration, 1, 0);
   }
-  
+
+  // Calculate and draw the gradient
+  drawGradient(lerpValue);
 }
-function randomRedOrPink() {
-    // Function to return a random red or pink color
-    let colors = [
-        color(255, 105, 180), // Hot pink
-        color(255, 182, 193), // Light pink
-        color(255, 0, 0),     // Red
-        color(255, 20, 147)   // Deep pink
-    ];
-    return random(colors);
+
+function drawGradient(t) {
+  for (let i = 0; i <= height; i++) {
+    let interA = lerpColor(colorStopsStart[0], colorStopsEnd[0], t);
+    let interB = lerpColor(colorStopsStart[1], colorStopsEnd[1], t);
+    let interC = lerpColor(colorStopsStart[2], colorStopsEnd[2], t);
+    
+    // Here we calculate the color at the current height position
+    let colorAtHeight = lerpColor(interA, interB, i / height);
+    colorAtHeight = lerpColor(colorAtHeight, interC, i / height);
+
+    stroke(colorAtHeight);
+    line(0, i, width, i);
+  }
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// Call the draw function every 30 milliseconds to update the canvas
+setInterval(() => {
+  loop();
+}, 30);
