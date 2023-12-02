a40eebac573a4db0ccb48763c381c6d901b1ad87
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-02 15:59:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,34 @@ const path = require('path');
 
 const app = express();
 
-// Update the SSL/TLS certificate paths to the new location
+// SSL/TLS certificate paths
 const options = {
   key: fs.readFileSync(path.join(__dirname, 'ssl_certs/privkey.pem')),
   cert: fs.readFileSync(path.join(__dirname, 'ssl_certs/fullchain.pem')),
   ca: fs.readFileSync(path.join(__dirname, 'ssl_certs/chain.pem'))
 };
 
-const fs = require('fs');
+// Function to determine the correct directory based on the hostname
+const chooseStaticDir = (req) => {
+  if (req.hostname === 'prestonbrubaker.com' || req.hostname === 'www.prestonbrubaker.com') {
+    return 'public_preston';
+  } else if (req.hostname === 'willohrobbins.com' || req.hostname === 'www.willohrobbins.com') {
+    return 'public_willoh';
+  }
+  return null;
+};
 
+// Serve files from the correct directory
+app.use((req, res, next) => {
+  const staticDir = chooseStaticDir(req);
+  if (staticDir) {
+    express.static(path.join(__dirname, staticDir))(req, res, next);
+  } else {
+    next(); // Continue to the next middleware if neither domain matches
+  }
+});
+
+// Logging Middleware
 app.use((req, res, next) => {
   const logEntry = {
     timestamp: new Date().toISOString(),
@@ -21,11 +40,10 @@ app.use((req, res, next) => {
     hostname: req.hostname
   };
 
-  // Read the current log file, add the new entry, and write it back
   fs.readFile('visit_logs.json', (err, data) => {
     let logs = [];
 
-    if (!err) {
+    if (!err && data) {
       try {
         logs = JSON.parse(data);
       } catch (parseErr) {
@@ -45,35 +63,15 @@ app.use((req, res, next) => {
   next();
 });
 
+// Fallback for any other requests
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
+
 // Define the port to run the server on
 const PORT = 443;
 
-// Function to determine the correct directory based on the hostname
-const chooseStaticDir = (req) => {
-  if (req.hostname === 'prestonbrubaker.com' || req.hostname === 'www.prestonbrubaker.com') {
-    return 'public_preston';
-  } else if (req.hostname === 'willohrobbins.com' || req.hostname === 'www.willohrobbins.com') {
-    return 'public_willoh';
-  }
-  return null;
-};
-
-// Use a middleware to serve files from the correct directory
-app.use((req, res, next) => {
-  const staticDir = chooseStaticDir(req);
-  if (staticDir) {
-    express.static(path.join(__dirname, staticDir))(req, res, next);
-  } else {
-    next(); // Continue to the next middleware if neither domain matches
-  }
-});
-
-// Fallback for any other requests
-app.use((req, res) => {
-    res.status(404).send('Page not found');
-});
-
-// Create an HTTPS server with the SSL/TLS options and attach the Express app
+// Create an HTTPS server and attach the Express app
 https.createServer(options, app).listen(PORT, () => {
-    console.log(`HTTPS server running on port ${PORT}`);
+  console.log(`HTTPS server running on port ${PORT}`);
 });
