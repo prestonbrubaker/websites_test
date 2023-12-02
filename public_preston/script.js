dafbac85ec7140059fa845b8f554a880d14ec35c
willohrobbins <willoh_robbins@outlook.com>
2023-12-02 01:21:10 -0600

    intermediary

--- public_preston/script.js
+++ public_preston/script.js
@@ -9,38 +9,37 @@ document.addEventListener('DOMContentLoaded', () => {
     var speed = 1;
     
     // Accessing the canvas element
-    var canvas = document.getElementById('canvas1');
+    var canvas = document.getElementById('iLoveYouPreston');
     var ctx = canvas.getContext('2d');
-
-    canvas.width = window.innerWidth * 0.9;
-    canvas.height = window.innerHeight * 0.9;
-    
-    var maxW = canvas1.width;
-    var maxH = canvas1.height;
-
+    canvas.width = window.innerWidth * 0.3;
+    canvas.height = window.innerHeight * 0.5;
+  
+    var maxW = canvas.width;
+    var maxH = canvas.height;
+  
     ctx.fillStyle = "#777777";
     ctx.fillRect(0, 0, maxW, maxH);
-
+  
     function incrementHue() {
         // Increment the hue
         hue = (hue + speed) % 360; // This will cycle hue from 0 to 359
-
+  
         // Set the fill color using HSL
         var color_rect = "hsl(" + hue + ", 100%, 50%)";
         
-
+  
         // Clear the canvas and draw a new rectangle
         ctx.clearRect(0, 0, maxW, maxH);
         ctx.fillStyle = "#777777";
         ctx.fillRect(0, 0, maxW, maxH);
-
-
+  
+  
         ctx.fillStyle = color_rect;
         ctx.fillRect(x, y, blockS, blockS);
-
+  
         x += xV * speed;
         y += yV * speed;
-
+  
         if(x > maxW - blockS || x < 0){
             xV *= -1;
         }
@@ -48,14 +47,15 @@ document.addEventListener('DOMContentLoaded', () => {
             yV *= -1;
         }
         speed += .001;
-
+  
         if(speed > 5){
             speed = 0;
         }
     }
-
+  
     
-
+  
     // Call incrementHue every 50 milliseconds (0.05 seconds)
     setInterval(incrementHue, 50);
-});
+  });
+  
