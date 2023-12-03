d03e861be49eec1743d19fe10531df1166f363ba
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-02 22:56:53 -0600

    Update script.js

--- public_markovian_developments/paint/script.js
+++ public_markovian_developments/paint/script.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    const blockSize = 5;
+    var blockSize = 5;
     const grid = 100;
     let hues = [];
 
