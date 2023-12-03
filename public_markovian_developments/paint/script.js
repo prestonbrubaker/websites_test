4eb893718db2e18f6c8e3f0048b5f7a2dd13a514
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-02 21:53:16 -0600

    Create script.js

---
+++ public_markovian_developments/paint/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    const blockSize = 100;
+    const grid = 5;
+    let hues = [];
+
+    function drawCanvas(hues) {
+        for (let i = 0; i < grid; i++) {
+            for (let j = 0; j < grid; j++) {
+                ctx.fillStyle = `hsl(${hues[i][j]}, 100%, 50%)`;
+                ctx.fillRect(j * blockSize, i * blockSize, blockSize, blockSize);
+            }
+        }
+    }
+
+    canvas.addEventListener('click', function(event) {
+        const x = Math.floor(event.offsetX / blockSize);
+        const y = Math.floor(event.offsetY / blockSize);
+        hues[y][x] = Math.floor(Math.random() * 360); // Random hue
+
+        drawCanvas(hues);
+        saveHues();
+    });
+
+    function saveHues() {
+        fetch('/save-canvas', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ hues: hues })
+        });
+    }
+
+    function loadHues() {
+        fetch('/get-canvas')
+            .then(response => response.json())
+            .then(data => {
+                hues = data.hues;
+                drawCanvas(hues);
+            });
+    }
+
+    loadHues();
+});
