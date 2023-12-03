b2ae02ffe946705aa9acd4ed7c52ce23bd620865
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-03 16:37:05 -0600

    Update three_body_problem.js

--- public_preston/simulations/three_body_problem/three_body_problem.js
+++ public_preston/simulations/three_body_problem/three_body_problem.js
@@ -6,9 +6,16 @@ var itC = 0;
 const tickS = 100;
 var maxW = c.width;   
 var maxH = c.height;
-var pixS = Math.floor(10);
+
+var blockSize = 10;
 
 const bgHue = "#777777";
+
+var x1 = 0;
+var y1 = 0;
+
+var x2 = .3;
+var y2 = 0;
 
 
 
@@ -18,11 +25,19 @@ function tick() {
     ctx.clearRect(0, 0, maxW, maxH);
     ctx.fillStyle = bgHue;
     ctx.fillRect(0, 0, maxW, maxH);
-
+    
     // Write troubleshooting info
     ctx.fillStyle = "#000000";
-
+    
     ctx.fillText("Program by Preston Brubaker", maxW / 2, maxH / 2);
+
+    ctx.fillStyle = "#FF0000";
+
+    ctx.fillRect(x1 * maxW - maxW / 2, y1 * maxH - maxH / 2, blockS, blockS);
+
+    ctx.fillStyle = "#00FF00";
+
+    ctx.fillRect(x2 * maxW - maxW / 2, y1 * maxH - maxH / 2, blockS, blockS);
 
 
     itC++;
