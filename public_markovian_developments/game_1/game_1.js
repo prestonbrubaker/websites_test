0c830590a04ba3ed65fb6794934ade4e2607bdeb
willohrobbins <willoh_robbins@outlook.com>
2023-12-04 21:26:52 -0600

    mobiles??

--- public_markovian_developments/game_1/game_1.js
+++ public_markovian_developments/game_1/game_1.js
@@ -170,6 +170,30 @@ document.addEventListener('DOMContentLoaded', function() {
         
     });
     
+    function setupMobileControls() {
+        var upButton = document.getElementById('up-button');
+        var downButton = document.getElementById('down-button');
+        var leftButton = document.getElementById('left-button');
+        var rightButton = document.getElementById('right-button');
+    
+        upButton.addEventListener('touchstart', function() {
+            if(offYP > 0) player_v_y -= player_acc_y;
+        });
+    
+        downButton.addEventListener('touchstart', function() {
+            if(offYP < pCYW - pCY) player_v_y += player_acc_y;
+        });
+    
+        leftButton.addEventListener('touchstart', function() {
+            if(offXP > 0) player_v_x -= player_acc_x;
+        });
+    
+        rightButton.addEventListener('touchstart', function() {
+            if(offXP < pCXW - pCX) player_v_x += player_acc_x;
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', setupMobileControls);
     
 
     loadHues();
