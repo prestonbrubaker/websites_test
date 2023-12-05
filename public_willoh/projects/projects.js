7a7990077fe85f7e6ce88fb3169143af1f9d94c2
willohrobbins <willoh_robbins@outlook.com>
2023-12-05 16:40:05 -0600

    lots of lil adjustments

--- public_willoh/projects/projects.js
+++ public_willoh/projects/projects.js
@@ -1,69 +1,27 @@
-let startTime;
-let colorStopsStart = []; // Start gradient colors
-let colorStopsEnd = []; // End gradient colors
+const text = "Previous Projects";
+let index = 0;
+let blinkIterations = 0;
+const maxBlinkIterations = 4; // Set to 4 or 5 as per your requirement
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noLoop(); // We will manually control the drawing loop
-  colorMode(RGB); // Use RGB color mode
-  startTime = millis();
-
-  // Manually define color stops for the starting ombre (sunrise)
-  colorStopsStart = [
-    // Replace these colors with those from your sunrise image
-    color(40, 100, 200), 
-    color(0, 150, 255),
-    color(64, 164, 223),   
-  ];
-
-  // Manually define color stops for the ending ombre (sunset)
-  colorStopsEnd = [
-    // Replace these colors with those from your sunset image
-    color(64, 164, 223), 
-    color(75, 119, 190), 
-    color(52, 92, 150),  
-  ];
+function typeText() {
+    if (index < text.length) {
+        document.getElementById("typing-effect").innerHTML += text.charAt(index);
+        index++;
+        const speed = 100 + Math.random() * 100; // Random speed between 100ms and 200ms
+        setTimeout(typeText, speed);
+    } else {
+        setTimeout(stopBlinking, 1000);
+    }
 }
 
-function draw() {
-  let cycleDuration = 60000; // 1 minute cycle for a full transition
-  let halfCycle = cycleDuration / 2; // Half cycle for transition back
-  let elapsedTime = (millis() - startTime) % cycleDuration;
-  let lerpValue;
-
-  if (elapsedTime < halfCycle) {
-    // First half of the cycle - transition from start to end colors
-    lerpValue = map(elapsedTime, 0, halfCycle, 0, 1);
-  } else {
-    // Second half of the cycle - transition from end back to start colors
-    lerpValue = map(elapsedTime, halfCycle, cycleDuration, 1, 0);
-  }
-
-  // Calculate and draw the gradient
-  drawGradient(lerpValue);
+function stopBlinking() {
+    if (blinkIterations < maxBlinkIterations) {
+        blinkIterations++;
+        setTimeout(stopBlinking, 1000);
+    } else {
+        const typingElement = document.getElementById("typing-effect");
+        typingElement.classList.add("no-blink");
+    }
 }
 
-
-function drawGradient(t) {
-  for (let i = 0; i <= height; i++) {
-    let interA = lerpColor(colorStopsStart[0], colorStopsEnd[0], t);
-    let interB = lerpColor(colorStopsStart[1], colorStopsEnd[1], t);
-    let interC = lerpColor(colorStopsStart[2], colorStopsEnd[2], t);
-    
-    // Here we calculate the color at the current height position
-    let colorAtHeight = lerpColor(interA, interB, i / height);
-    colorAtHeight = lerpColor(colorAtHeight, interC, i / height);
-
-    stroke(colorAtHeight);
-    line(0, i, width, i);
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-// Call the draw function every 30 milliseconds to update the canvas
-setInterval(() => {
-  loop();
-}, 30);
+window.onload = typeText;
--- public_willoh/script.js
+++ public_willoh/script.js
@@ -1,94 +1,27 @@
-let startTime;
-let colorStopsStart = []; // Start gradient colors
-let colorStopsEnd = []; // End gradient colors
-let stars = []; // Array to hold the stars
+const text = "Hello. I'm Willoh!";
+let index = 0;
+let blinkIterations = 0;
+const maxBlinkIterations = 4; // Set to 4 or 5 as per your requirement
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  colorMode(RGB);
-  startTime = millis();
-
-  // Manually define color stops for the starting ombre (sunrise)
-  colorStopsStart = [
-    color(40, 100, 200), // Replace with sunrise colors
-    color(0, 150, 255),
-    color(64, 164, 223),
-  ];
-
-  // Manually define color stops for the ending ombre (sunset)
-  colorStopsEnd = [
-    color(64, 164, 223), // Replace with sunset colors
-    color(75, 119, 255),
-    color(52, 92, 255),
-  ];
-
-  // Define the boundaries of the text box to avoid placing stars
-  let textBoxTop = height / 2 - 50;
-  let textBoxBottom = height / 2 + 50;
-  let textBoxLeft = width / 2 - 100;
-  let textBoxRight = width / 2 + 100;
-
-  // Add stars around the text, avoiding the rectangular area in the middle
-  for (let i = 0; i < 100; i++) {
-    let x = random(width);
-    let y = random(height);
-    let size = random(1, 3);
-    if (y < textBoxTop || y > textBoxBottom || x < textBoxLeft || x > textBoxRight) {
-      stars.push(new Star(x, y, size));
+function typeText() {
+    if (index < text.length) {
+        document.getElementById("typing-effect").innerHTML += text.charAt(index);
+        index++;
+        const speed = 100 + Math.random() * 100; // Random speed between 100ms and 200ms
+        setTimeout(typeText, speed);
+    } else {
+        setTimeout(stopBlinking, 1000);
     }
-  }
 }
 
-function draw() {
-  let cycleDuration = 60000; // 1 minute cycle for a full transition
-  let halfCycle = cycleDuration / 2; // Half cycle for transition back
-  let elapsedTime = (millis() - startTime) % cycleDuration;
-  let lerpValue = elapsedTime < halfCycle ? map(elapsedTime, 0, halfCycle, 0, 1) : map(elapsedTime, halfCycle, cycleDuration, 1, 0);
-
-  drawGradient(lerpValue);
-
-  // Display the stars
-  for (let star of stars) {
-    star.display();
-  }
+function stopBlinking() {
+    if (blinkIterations < maxBlinkIterations) {
+        blinkIterations++;
+        setTimeout(stopBlinking, 1000);
+    } else {
+        const typingElement = document.getElementById("typing-effect");
+        typingElement.classList.add("no-blink");
+    }
 }
 
-function drawGradient(t) {
-  for (let i = 0; i <= height; i++) {
-    let interA = lerpColor(colorStopsStart[0], colorStopsEnd[0], t);
-    let interB = lerpColor(colorStopsStart[1], colorStopsEnd[1], t);
-    let interC = lerpColor(colorStopsStart[2], colorStopsEnd[2], t);
-
-    let colorAtHeight = lerpColor(interA, interB, i / height);
-    colorAtHeight = lerpColor(colorAtHeight, interC, i / height);
-
-    stroke(colorAtHeight);
-    line(0, i, width, i);
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-// Define the Star class
-class Star {
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.twinkleFactor = random(150, 255);
-  }
-
-  display() {
-    noStroke();
-    fill(255, this.twinkleFactor);
-    circle(this.x, this.y, this.size);
-    this.twinkle();
-  }
-
-  twinkle() {
-    this.twinkleFactor += random(-20, 20);
-    this.twinkleFactor = constrain(this.twinkleFactor, 150, 255);
-  }
-}
+window.onload = typeText;
