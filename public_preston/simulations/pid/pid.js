2c88670dd1212f78f762c73475918d9d5c81f7e1
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-05 17:02:28 -0600

    Update pid.js

--- public_preston/simulations/pid/pid.js
+++ public_preston/simulations/pid/pid.js
@@ -268,7 +268,7 @@ function tick() {
     
     // add heat to heater
     if(vessel_cont_temp < 75){
-        qdot = 300000;
+        qdot = 100000;
         heater_temp += qdot / mass_heater / heat_cap_steel * dt;
     }
     
