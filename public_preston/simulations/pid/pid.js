1b077e356d2efc26fb16d0a43b259f772c3486f6
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-05 16:51:01 -0600

    Update pid.js

--- public_preston/simulations/pid/pid.js
+++ public_preston/simulations/pid/pid.js
@@ -213,16 +213,16 @@ function tick() {
 
     //plot temps
     ctx3.fillStyle = "#FF0000";
-    ctx3.fillRect(itC / 5000 * maxW, maxH - heater_temp * 3, 2, 2)
+    ctx3.fillRect(itC / 5000 * maxW, maxH - (heater_temp - 20) * 3, 2, 2)
     
     ctx3.fillStyle = "#FFFF00";
-    ctx3.fillRect(itC / 5000 * maxW, maxH - fluid_temp * 3, 2, 2)
+    ctx3.fillRect(itC / 5000 * maxW, maxH - (heater_temp - 20) * 3, 2, 2)
 
     ctx3.fillStyle = "#00FF00";
-    ctx3.fillRect(itC / 5000 * maxW, maxH - vessel_wall_temp * 3, 2, 2)
+    ctx3.fillRect(itC / 5000 * maxW, maxH - (heater_temp - 20) * 3, 2, 2)
 
     ctx3.fillStyle = "#0000FF";
-    ctx3.fillRect(itC / 5000 * maxW, maxH - vessel_cont_temp * 3, 2, 2)
+    ctx3.fillRect(itC / 5000 * maxW, maxH - (heater_temp - 20) * 3, 2, 2)
 
     // Show the temps on the PFD
     var heater_l = 0;
