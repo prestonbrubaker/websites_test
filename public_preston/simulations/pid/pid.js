61f8d973ad99d0492125d7cc153ad7d17fbeee90
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-06 17:15:09 -0600

    Update pid.js

--- public_preston/simulations/pid/pid.js
+++ public_preston/simulations/pid/pid.js
@@ -360,7 +360,7 @@ function tick() {
             ctx.fillStyle = "hsl(0, 50%, " + 5 + "%)";
         }
     }else if (method == 3){    // PID Control
-        multiplier = (setpoint_temp - vessel_cont_temp) / 50 - 800 * derivative - integral / 4000;
+        multiplier = (setpoint_temp - vessel_cont_temp) / 50 - 600 * derivative - integral / 4000;
         if(multiplier > 1){
             qdot = max_q;
         } else if (multiplier > 0){
