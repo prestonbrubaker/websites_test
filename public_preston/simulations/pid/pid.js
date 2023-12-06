866c64566f32e44de5a048f7e93691e157b196b1
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-06 14:49:38 -0600

    Update pid.js

--- public_preston/simulations/pid/pid.js
+++ public_preston/simulations/pid/pid.js
@@ -217,7 +217,7 @@ function tick() {
     ctx3.fillRect(0,0,maxW,150);
 
     ctx.fillStyle = "#333333";
-    ctx.fillRect(0,150,maxW,2);
+    ctx.fillRect(0,150,maxW,-2);
     
     ctx3.fillStyle = "#FFFFFF";
     
