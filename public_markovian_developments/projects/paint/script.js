048f386b9fb3310dde7a32c9173f70478d5b21f0
willohrobbins <willoh_robbins@outlook.com>
2023-12-08 12:10:15 -0600

    toggle dropdown

--- public_markovian_developments/projects/paint/script.js
+++ public_markovian_developments/projects/paint/script.js
@@ -1,4 +1,18 @@
+document.body.style.cursor = 'url(../images/select.png), auto';
 
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownMenu");
+    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdwn')) {
+        var dropdownContent = document.getElementById("dropdownMenu");
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        }
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('canvas1');
