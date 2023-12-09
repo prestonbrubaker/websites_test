790208fdf19ac533db930eda852c70afd39b8240
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-09 15:03:24 -0600

    Update game_2.js

--- public_markovian_developments/projects/game_2/game_2.js
+++ public_markovian_developments/projects/game_2/game_2.js
@@ -15,19 +15,13 @@ window.onclick = function(event) {
 var canvas = document.getElementById('canvas1');
 var ctx = canvas.getContext('2d');
 const grid = 100;
-let hues = new Array(grid).fill().map(() => new Array(grid).fill(0)); // Initialize the hues array
+let hues = new Array(grid).fill().map(() => new Array(grid).fill(0));
 
-// Set canvas size
-var maxW = canvas.width;
-var maxH = canvas.height;
+canvas.width = 500;
+canvas.height = 500;
+var blockSize = Math.min(canvas.width, canvas.height) / grid;
 
-// Calculate block size
-var blockSize = Math.min(maxW, maxH) / grid;
-if (blockSize < 1) {
-    blockSize = 1;
-}
-
-function drawCanvas(hues) {
+function drawCanvas() {
     for (let i = 0; i < grid; i++) {
         for (let j = 0; j < grid; j++) {
             ctx.fillStyle = `hsl(${hues[i][j]}, 100%, 50%)`;
@@ -39,30 +33,8 @@ function drawCanvas(hues) {
 canvas.addEventListener('click', function(event) {
     const x = Math.floor(event.offsetX / blockSize);
     const y = Math.floor(event.offsetY / blockSize);
-    hues[y][x] = Math.floor(Math.random() * 360); // Random hue
-
-    drawCanvas(hues);
-    saveHues();
+    hues[y][x] = Math.floor(Math.random() * 360);
+    drawCanvas();
 });
 
-function saveHues() {
-    fetch('/save-game_2', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ hues: hues })
-    });
-}
-
-function loadHues() {
-    fetch('/get-game_2')
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.hues) {
-                hues = data.hues;
-                drawCanvas(hues);
-            }
-        });
-}
-
-loadHues();
-setInterval(loadHues, 2000);
+drawCanvas(); // Initial draw to fill the canvas
