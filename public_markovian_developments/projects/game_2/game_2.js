2541623928685703a23481e1c5412633768a5ee3
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-09 15:02:01 -0600

    Update game_2.js

--- public_markovian_developments/projects/game_2/game_2.js
+++ public_markovian_developments/projects/game_2/game_2.js
@@ -12,55 +12,57 @@ window.onclick = function(event) {
     }
 }
 
+var canvas = document.getElementById('canvas1');
+var ctx = canvas.getContext('2d');
+const grid = 100;
+let hues = new Array(grid).fill().map(() => new Array(grid).fill(0)); // Initialize the hues array
 
-    var canvas = document.getElementById('canvas1');
-    var ctx = canvas.getContext('2d');
-    var blockSize = 5;
-    const grid = 100;
-    let hues = [];
+// Set canvas size
+var maxW = canvas.width;
+var maxH = canvas.height;
 
-    var maxW = 500
-    var maxH = 500
+// Calculate block size
+var blockSize = Math.min(maxW, maxH) / grid;
+if (blockSize < 1) {
+    blockSize = 1;
+}
 
-    blockS = maxH / grid
-
-
-    function drawCanvas(hues) {
-        for (let i = 0; i < grid; i++) {
-            for (let j = 0; j < grid; j++) {
-                ctx.fillStyle = `hsl(${hues[i][j]}, 100%, 50%)`;
-                ctx.fillRect(j * blockSize, i * blockSize, blockSize, blockSize);
-            }
+function drawCanvas(hues) {
+    for (let i = 0; i < grid; i++) {
+        for (let j = 0; j < grid; j++) {
+            ctx.fillStyle = `hsl(${hues[i][j]}, 100%, 50%)`;
+            ctx.fillRect(j * blockSize, i * blockSize, blockSize, blockSize);
         }
-
     }
+}
 
-    canvas.addEventListener('click', function(event) {
-        const x = Math.floor(event.offsetX / blockSize);
-        const y = Math.floor(event.offsetY / blockSize);
-        hues[y][x] = Math.floor(Math.random() * 360); // Random hue
+canvas.addEventListener('click', function(event) {
+    const x = Math.floor(event.offsetX / blockSize);
+    const y = Math.floor(event.offsetY / blockSize);
+    hues[y][x] = Math.floor(Math.random() * 360); // Random hue
 
-        drawCanvas(hues);
-        saveHues();
+    drawCanvas(hues);
+    saveHues();
+});
+
+function saveHues() {
+    fetch('/save-game_2', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ hues: hues })
     });
+}
 
-    function saveHues() {
-        fetch('/save-game_2', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ hues: hues })
-        });
-    }
-
-    function loadHues() {
-        fetch('/get-game_2')
-            .then(response => response.json())
-            .then(data => {
+function loadHues() {
+    fetch('/get-game_2')
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.hues) {
                 hues = data.hues;
                 drawCanvas(hues);
-            });
-    }
+            }
+        });
+}
 
-    loadHues();
-    setInterval(loadHues, 2000);
-
+loadHues();
+setInterval(loadHues, 2000);
