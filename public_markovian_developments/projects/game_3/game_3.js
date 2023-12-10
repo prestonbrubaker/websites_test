032b48cb0c76a6419799d261bda0774461931325
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-10 00:05:27 -0600

    Update game_3.js

--- public_markovian_developments/projects/game_3/game_3.js
+++ public_markovian_developments/projects/game_3/game_3.js
@@ -2,6 +2,8 @@
 
     var canvas = document.getElementById('canvas1');
     var ctx = canvas.getContext('2d');
+    var canvas2 = document.getElementById('canvas2');
+    var ctx2 = canvas2.getContext('2d');
     var blockSize = 5;
     const grid = 3;
     let hues = [];
