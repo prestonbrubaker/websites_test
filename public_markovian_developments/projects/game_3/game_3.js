72e1332d6a456261db241a7da3dff423e1091f1c
willohrobbins <willoh_robbins@outlook.com>
2023-12-10 16:31:28 -0600

    buttons

--- public_markovian_developments/projects/game_3/game_3.js
+++ public_markovian_developments/projects/game_3/game_3.js
@@ -1,4 +1,16 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownMenu");
+    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+}
 
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdwn')) {
+        var dropdownContent = document.getElementById("dropdownMenu");
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        }
+    }
+}
 
     var canvas = document.getElementById('canvas1');
     var ctx = canvas.getContext('2d');
