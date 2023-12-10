df07a964f14327cb68f7301a19ffdbd4d738e03c
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-10 00:30:05 -0600

    Update game_3.js

--- public_markovian_developments/projects/game_3/game_3.js
+++ public_markovian_developments/projects/game_3/game_3.js
@@ -59,11 +59,12 @@
         sum_sq_time_lost += time_lost ** 2;
         comp_speed = sum_sq_time_lost / time_epoch
         ctx2.fillStyle = "#000000";
-        ctx2.fillText("Game Time: " + time,10,10);
-        ctx2.fillText("Actual Time: " + time_epoch,10,30);
-        ctx2.fillText("Lost Time: " + time_lost,10,50);
-        ctx2.fillText("Sum Squared of Lost Time: " + sum_sq_time_lost,10,70);
-        ctx2.fillText("Computer Fittness: " + comp_speed,10,90);
+        ctx2.fillText("Epoch Time: " + Date.now(),10,10);
+        ctx2.fillText("Game Time: " + time,10,30);
+        ctx2.fillText("Actual Time: " + time_epoch,10,50);
+        ctx2.fillText("Lost Time: " + time_lost,10,70);
+        ctx2.fillText("Sum Squared of Lost Time: " + sum_sq_time_lost,10,90);
+        ctx2.fillText("Computer Fittness: " + comp_speed,10,110);
         if(Date.now() % Math.floor(hues[0][0]) < Math.floor(hues[0][0] / 2)){
             ctx2.fillStyle = "#000000";
         } else{
