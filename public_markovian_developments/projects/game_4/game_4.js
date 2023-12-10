2f10d272502272b12ec08f0a712f6bd2d2aa2d18
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-10 17:48:14 -0600

    Update game_4.js

--- public_markovian_developments/projects/game_4/game_4.js
+++ public_markovian_developments/projects/game_4/game_4.js
@@ -45,10 +45,10 @@ window.onclick = function(event) {
     }
 
     function drawCanvas(hues) {
-        for (let i = 0; i < grid; i++) {
-            for (let j = 0; j < 1; j++) {
+        for (let i = 0; i < grid / 2; i++) {
+            for (let j = 0; j < 2; j++) {
                 ctx.fillStyle = `hsl(${hues[i][j]}, 100%, 50%)`;
-                ctx.fillRect(j * blockSize, i * blockSize, maxW, blockSize);
+                ctx.fillRect(j * blockSize, i * blockSize, maxW / 2, blockSize);
             }
         }
         ctx.fillStyle = "#FFFFFF";
@@ -56,8 +56,8 @@ window.onclick = function(event) {
     }
 
     canvas.addEventListener('click', function(event) {
-        const x = 0;
-        const y = Math.floor(event.offsetY / blockSize);
+        const x = Math.floor(event.offsetX / maxW * 2);
+        const y = Math.floor(event.offsetY / blockSize / 2);
         if(hue_sto == -1){
             hues[y][x] = Math.floor(Math.random() * 360); // Random hue
         } else {
