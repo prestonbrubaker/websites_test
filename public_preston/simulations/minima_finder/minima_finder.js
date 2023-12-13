c6ab6acdb02e6e7a39cddaa5c86f378f812532d9
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-12-12 18:30:04 -0600

    Update minima_finder.js

--- public_preston/simulations/minima_finder/minima_finder.js
+++ public_preston/simulations/minima_finder/minima_finder.js
@@ -210,7 +210,7 @@ function tick2() {
     }
 
     ctx3.fillStyle = "#FFFFFF";
-    ctx3.fillRect(itC / 20000 * maxW + 10, maxH - Math.log(sse_c) * 20 - 100,2,2);
+    ctx3.fillRect(itC / 20000 * maxW + 10, maxH - Math.log(sse_c) * 20 - 200,2,2);
     
 }
 
